5993b8824836813a4b29476d59f67ac2b285efb1
Stew Eucen <stew.eucen@gmail.com>
2023-05-23 18:52:23 +0900

    Add test for LintKeyExtractor#get:ruleId

--- tests/__tests__/tools/LintKeyExtractor.js
+++ tests/__tests__/tools/LintKeyExtractor.js
@@ -72,3 +72,83 @@ describe('LintKeyExtractor', () => {
     })
   })
 })
+
+describe('LintKeyExtractor', () => {
+  describe('#get:ruleId', () => {
+    describe('with ruleId only', () => {
+      const cases = [
+        {
+          params: {
+            filePath: '/root/tests/targets/standard/indent.js',
+          },
+          expected: 'indent',
+        },
+        {
+          params: {
+            filePath: '/root/tests/targets/standard/semi.js',
+          },
+          expected: 'semi',
+        },
+        {
+          params: {
+            filePath: '/root/tests/targets/standard/no-restricted-syntax.js',
+          },
+          expected: 'no-restricted-syntax',
+        },
+      ]
+
+      test.each(cases)('filePath: $params.filePath', ({ params, expected }) => {
+        const extractor = LintKeyExtractor.create(params)
+
+        expect(extractor.ruleId)
+          .toBe(expected)
+      })
+    })
+
+    describe('with ruleId "no-restricted-syntax"', () => {
+      const cases = [
+        {
+          params: {
+            filePath: '/root/tests/targets/standard/no-restricted-syntax/noLet.js',
+          },
+          expected: 'no-restricted-syntax',
+        },
+        {
+          params: {
+            filePath: '/root/tests/targets/standard/no-restricted-syntax/noArrayForEach.js',
+          },
+          expected: 'no-restricted-syntax',
+        },
+        {
+          params: {
+            filePath: '/root/tests/targets/standard/no-restricted-syntax/loop-controls/noArrayForEach.js',
+          },
+          expected: 'no-restricted-syntax',
+        },
+      ]
+
+      test.each(cases)('filePath: $params.filePath', ({ params, expected }) => {
+        const extractor = LintKeyExtractor.create(params)
+
+        expect(extractor.ruleId)
+          .toBe(expected)
+      })
+    })
+
+    describe('invalid file path', () => {
+      const cases = [
+        { params: { filePath: '<text>' } },
+        { params: { filePath: '' } },
+        { params: { filePath: null } },
+        { params: { filePath: undefined } },
+      ]
+
+      test.each(cases)('filePath: $params.filePath', ({ params }) => {
+        const extractor = LintKeyExtractor.create(params)
+
+        expect(extractor.ruleId)
+          .toBeNull()
+      })
+    })
+  })
+})
