07762fc3cc69935698618f332e84c868815df15a
Stew Eucen <stew.eucen@gmail.com>
2024-05-05 19:36:41 +0900

    Update ignores as global in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -17,8 +17,19 @@ module.exports = [
       },
       sourceType: 'commonjs',
     },
+  },
+
+  /*
+   * If ignores is used without any other keys in the configuration object, then the patterns act as global ignores. Here’s an example:
+   *
+   * https://eslint.org/docs/latest/use/configure/configuration-files#globally-ignoring-files-with-ignores
+   */
+  {
     ignores: [
       '**/node_modules/**',
+      '**/tests/resources/expected/**',
+      '**/tests/resources/no-lints/**',
+      '**/tests/resources/unexpected/**',
     ],
   },
 
